b47c79cb710a9bcf2913ec21dc3d9ac6acc817b5
Tim Galloway <gallowaytim70@gmail.com>
2017-02-10 15:50:36 +0800

    Some StyleCop changes

--- DailyContact/MainActivity.cs
+++ DailyContact/MainActivity.cs
@@ -1,110 +1,213 @@
-﻿using System;
-using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
-using Android.Telephony;
-using Android.Locations;
-using System.Collections.Generic;
-using System.Linq;
-using Android.Util;
-using System.Threading.Tasks;
-using System.Text;
-using DailyContact;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="MainActivity.cs" company="GallowayConsulting">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
 namespace DailyContact
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Android.App;
+    using Android.Content;
+    using Android.Locations;
+    using Android.OS;
+    using Android.Telephony;
+    using Android.Util;
+    using Android.Widget;
+
+    /// <summary>
+    /// Main Activity Class
+    /// </summary>
     [Activity(Label = "DailyContact", MainLauncher = true, Icon = "@drawable/PSC4wd")]
     public class MainActivity : Activity, ILocationListener
     {
-        Location _currentLocation;
+        /// <summary>
+        /// Current Location of the device
+        /// </summary>
+        Location currentLocation;
 
+        /// <summary>
+        /// Physical address of the device
+        /// </summary>
+        TextView addressText;
+
+        /// <summary>
+        /// Location manager class
+        /// </summary>
+        LocationManager locationManager;
+
+        /// <summary>
+        /// String represting the location provider
+        /// </summary>
+        string locationProvider;
+
+        /// <summary>
+        /// object that is the lat long text
+        /// </summary>
+        TextView latLongText;
+
+        /// <summary>
+        /// Method that is run when location changes
+        /// </summary>
+        /// <param name="location"></param>
         public void OnLocationChanged(Location location)
         {
-            _currentLocation = location;
-            if (_currentLocation == null)
+            this.currentLocation = location;
+            if (this.currentLocation == null)
             {
-                _LatLongText.Text = "Cannot find current location";
-            }else
-            {
-                _LatLongText.Text = "Lat: " +_currentLocation.Latitude.ToString() + ": Long: " + _currentLocation.Longitude.ToString();
-
-                // Get our button from the layout resource,
-                // and attach an event to it
-                Button bSendSMS = FindViewById<Button>(Resource.Id.btnSendSMS);
-                Button bGetLocation = FindViewById<Button>(Resource.Id.btnLocation);
-                bSendSMS.Enabled = true;
-                bGetLocation.Enabled = true;
-            }
-        }
-
-        public void OnProviderDisabled(string provider) { }
-
-        public void OnProviderEnabled(string provider) { }
-
-        public void OnStatusChanged(string provider, Availability status, Bundle extras) { }
-        
-        //static readonly string TAG = "X:" + typeof(Activity1).Name;
-        TextView _addressText;
-        LocationManager _locationManager;
-
-        string _locationProvider;
-        TextView _LatLongText;
-
-        void InitializeLocationManager()
-        {
-            _locationManager = (LocationManager)GetSystemService(LocationService);
-            Criteria criteriaForLocationService = new Criteria
-            {
-                Accuracy = Accuracy.Fine
-            };
-            IList<string> acceptableLocationProviders = _locationManager.GetProviders(criteriaForLocationService, true);
-
-            if (acceptableLocationProviders.Any())
-            {
-                _locationProvider = acceptableLocationProviders.First();
+                this.latLongText.Text = "Cannot find current location";
             }
             else
             {
-                _locationProvider = string.Empty;
+                this.latLongText.Text = "Lat: " + this.currentLocation.Latitude.ToString() + ": Long: " + this.currentLocation.Longitude.ToString();
+
+                // Get our button from the layout resource,
+                // and attach an event to it
+                Button sendSMS = FindViewById<Button>(Resource.Id.btnSendSMS);
+                Button getLocation = FindViewById<Button>(Resource.Id.btnLocation);
+                sendSMS.Enabled = true;
+                getLocation.Enabled = true;
             }
-            Log.Debug("TAG", "Using " + _locationProvider + ".");
         }
 
+        public void OnProviderDisabled(string provider)
+        {
+        }
+
+        public void OnProviderEnabled(string provider)
+        {
+        }
+
+        public void OnStatusChanged(string provider, Availability status, Bundle extras)
+        {
+        }
+        
         protected override void OnResume()
         {
             base.OnResume();
-            _locationManager.RequestLocationUpdates(_locationProvider, 0, 0, this);
+            this.locationManager.RequestLocationUpdates(this.locationProvider, 0, 0, this);
         }
 
         protected override void OnPause()
         {
             base.OnPause();
-            _locationManager.RemoveUpdates(this);
+            this.locationManager.RemoveUpdates(this);
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            this.SetContentView(Resource.Layout.Main);
+
+            // Set AppPreferences object
+            Context context = Android.App.Application.Context;
+            AppPreferences ap = new AppPreferences(context);
+
+            // Get current phone numbers 
+            string phonenumbers = ap.getAccessKey();
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button sendSMS = FindViewById<Button>(Resource.Id.btnSendSMS);
+            Button getLocation = FindViewById<Button>(Resource.Id.btnLocation);
+            EditText phoneNumbers = FindViewById<EditText>(Resource.Id.txtPhoneNumbers);
+
+            phoneNumbers.Text = phonenumbers;
+
+            // Get the text fields from the layout
+            this.addressText = this.FindViewById<TextView>(Resource.Id.txtLocation);
+            this.latLongText = this.FindViewById<TextView>(Resource.Id.txtLatLong);
+
+            // Disable buttons until location is found
+            sendSMS.Enabled = false;
+            getLocation.Enabled = false;
+
+            this.InitializeLocationManager();
+
+            sendSMS.Click += delegate
+            {
+                var phoneNumbersText = FindViewById<TextView>(Resource.Id.txtPhoneNumbers);
+                var situationRB = FindViewById<RadioGroup>(Resource.Id.radioGroup1);
+                var situationButton = FindViewById<RadioButton>(situationRB.CheckedRadioButtonId);
+
+                // fetch the Sms Manager
+                SmsManager sms = SmsManager.Default;
+
+                string strSMS = "Current Lat/Long: " + this.currentLocation.Latitude.ToString() + " / " + this.currentLocation.Longitude.ToString() +
+                                    "\r\n\r\nLocation: " + this.addressText.Text +
+                                    "\r\n\r\nSituation: " + situationButton.Text;
+
+                // split it between any commas, stripping whitespace afterwards
+                string userInput = phoneNumbersText.Text.ToString();
+                string[] numbers = userInput.Split(',');
+
+                foreach (string number in numbers)
+                {
+                    sms.SendTextMessage(number, null, strSMS, null, null);
+                }
+
+                ap.saveAccessKey(userInput);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.SetTitle("Alert");
+                builder.SetMessage("Message(s) sent");
+                builder.SetCancelable(false);
+                builder.SetPositiveButton("OK", delegate { this.Finish(); });
+                builder.Show();
+            };
+
+            getLocation.Click += delegate
+            {
+                this.BtnLocation_OnClick();
+            };
+        }
+
+        void InitializeLocationManager()
+        {
+            this.locationManager = (LocationManager)GetSystemService(LocationService);
+            Criteria criteriaForLocationService = new Criteria
+            {
+                Accuracy = Accuracy.Fine
+            };
+            IList<string> acceptableLocationProviders = this.locationManager.GetProviders(criteriaForLocationService, true);
+
+            if (acceptableLocationProviders.Any())
+            {
+                this.locationProvider = acceptableLocationProviders.First();
+            }
+            else
+            {
+                this.locationProvider = string.Empty;
+            }
+
+            Log.Debug("TAG", "Using " + this.locationProvider + ".");
         }
 
         async Task<Address> ReverseGeocodeCurrentLocation()
         {
             Geocoder geocoder = new Geocoder(this);
             IList<Address> addressList =
-                await geocoder.GetFromLocationAsync(_currentLocation.Latitude, _currentLocation.Longitude, 10);
+                await geocoder.GetFromLocationAsync(this.currentLocation.Latitude, this.currentLocation.Longitude, 10);
 
             Address address = addressList.FirstOrDefault();
             return address;
         }
 
-        async void btnLocation_OnClick()
+        async void BtnLocation_OnClick()
         {
-            if (_currentLocation == null)
+            if (this.currentLocation == null)
             {
-                _addressText.Text = "Can't determine the current address. Try again in a few minutes.";
+                this.addressText.Text = "Can't determine the current address. Try again in a few minutes.";
                 return;
             }
 
-            Address address = await ReverseGeocodeCurrentLocation();
-            DisplayAddress(address);
+            Address address = await this.ReverseGeocodeCurrentLocation();
+            this.DisplayAddress(address);
         }
 
         void DisplayAddress(Address address)
@@ -116,84 +219,14 @@ namespace DailyContact
                 {
                     deviceAddress.AppendLine(address.GetAddressLine(i));
                 }
+
                 // Remove the last comma from the end of the address.
-                _addressText.Text = deviceAddress.ToString();
+                this.addressText.Text = deviceAddress.ToString();
             }
             else
             {
-                _addressText.Text = "Unable to determine the address. Try again in a few minutes.";
+                this.addressText.Text = "Unable to determine the address. Try again in a few minutes.";
             }
-        }
-
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
-
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
-
-            // Set AppPreferences object
-            Context mContext = Android.App.Application.Context;
-            AppPreferences ap = new AppPreferences(mContext);
-
-            // Get current phone numbers 
-            string _phonenumbers = ap.getAccessKey();
-
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button bSendSMS = FindViewById<Button>(Resource.Id.btnSendSMS);
-            Button bGetLocation = FindViewById<Button>(Resource.Id.btnLocation);
-            EditText tPhoneNumbers = FindViewById<EditText>(Resource.Id.txtPhoneNumbers);
-
-            tPhoneNumbers.Text = _phonenumbers;
-
-            // Get the text fields from the layout
-            _addressText = FindViewById<TextView>(Resource.Id.txtLocation);
-            _LatLongText = FindViewById<TextView>(Resource.Id.txtLatLong);
-
-            //Disable buttons until location is found
-            bSendSMS.Enabled = false;
-            bGetLocation.Enabled = false;
-
-            InitializeLocationManager();
-
-            bSendSMS.Click += delegate {
-                var _PhoneNumbersText = FindViewById<TextView>(Resource.Id.txtPhoneNumbers);
-                var _SituationRB = FindViewById<RadioGroup>(Resource.Id.radioGroup1);
-                var _SituationButton = FindViewById<RadioButton>(_SituationRB.CheckedRadioButtonId);
-
-                // fetch the Sms Manager
-                SmsManager sms = SmsManager.Default;
-
-                String _SMSString = "Current Lat/Long: " + _currentLocation.Latitude.ToString() + " / " + _currentLocation.Longitude.ToString() +
-                                    "\r\n\r\nLocation: " + _addressText.Text +
-                                    "\r\n\r\nSituation: " + _SituationButton.Text;
-
-                // split it between any commas, stripping whitespace afterwards
-                String userInput = _PhoneNumbersText.Text.ToString();
-                String[] numbers = userInput.Split(',');
-
-                foreach (string number in numbers)
-                {
-                    sms.SendTextMessage(number, null, _SMSString, null, null);
-                }
-
-                ap.saveAccessKey(userInput);
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.SetTitle("Alert");
-                builder.SetMessage("Message(s) sent");
-                builder.SetCancelable(false);
-                builder.SetPositiveButton("OK", delegate { Finish(); });
-                builder.Show();
-            };
-
-            bGetLocation.Click += delegate
-            {
-                btnLocation_OnClick();
-            };
-
         }
     }
 }
-
